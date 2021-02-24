e2d95a24daeb38fe1eacf76d548645bc82ce57e9
michael-williamson <michaeldavid1721@yahoo.com>
2021-02-24 08:06:11 -0600

    commented out computations

--- js/app.js
+++ js/app.js
@@ -1,103 +1,71 @@
-'use strict'
+"use strict";
 
+function rotate() {
+  let r = 150;
+  let rDiff = 200;
 
+  $("#orbit-ring li").each(function () {
+    let pos = $(this).position();
 
-function rotate(){
-
-    let r = 150; 
-
-  $("#orbit-ring li").each(function(){
-     let pos = $(this).position();
-     
     let elemPosX = pos.left;
-     let elemPosY = pos.top;
+    let elemPosY = pos.top;
 
-      let angle = Math.atan2(elemPosY - 150,elemPosX - 150);
+    let angle = Math.atan2(elemPosY - 150, elemPosX - 150);
 
-          angle += 0.001;
+    angle += 0.8;
 
-       let y = Math.sin(angle) * r + 150;
-    
-     let x = Math.cos(angle) * r + 150;
+    let y = Math.sin(angle) * r + 150;
 
+    let x = Math.cos(angle) * r + 150;
 
+    let yPixel = `${y}px`;
+    let xPixel = `${x}px`;
 
-    let yPixel =`${y}px` 
-    let xPixel = `${x}px`
-
-      this.style.left = xPixel;
-    this.style.top = yPixel; 
+    this.style.left = xPixel;
+    this.style.top = yPixel;
   });
 }
 
+function placement() {
+  let plots = 9;
 
-function placement(){
-    let plots = 9; 
+  let increase = (Math.PI * 2) / plots;
 
-    let increase = Math.PI * 2/plots; 
+  let angle = 0;
 
-    let angle = 0; 
+  $("#orbit-ring li").each(function () {
+    let r = 200;
+    let rDiff = 200;
 
-    $("#orbit-ring li").each(function(){
-     let r = 150; 
-    
-    
-    let y = Math.sin(angle) * r + 150;
-    
-     let x = Math.cos(angle) * r + 150;
+    let y = Math.sin(angle) * r + rDiff;
 
-   
-    let yPixel =`${y}px` 
-    let xPixel = `${x}px`
+    let x = Math.cos(angle) * r + rDiff;
 
-      this.style.left = xPixel;
-    this.style.top = yPixel; 
+    let yPixel = `${y}px`;
+    let xPixel = `${x}px`;
 
-    angle += increase; 
-    });
+    this.style.left = xPixel;
+    this.style.top = yPixel;
+
+    angle += increase;
+  });
 }
 
 placement();
 
-window.setInterval(rotate,5);
+// window.setInterval(rotate, 2000);
 
+//Javascript clock
 
-
-//Javascript clock 
-
-function clock(){
-
+function clock() {
   let currentTime = new Date();
 
   currentTime = currentTime.toString();
-  currentTime = currentTime.substr(15,9);
-  
-    $("#clock p").text(currentTime);
+  currentTime = currentTime.substr(15, 9);
 
+  $("#clock p").text(currentTime);
 }
 
-clock(); 
+clock();
 
-window.setInterval(clock,1000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+window.setInterval(clock, 1000);
